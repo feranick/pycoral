f73724866678a83ee836fc04215000412b3ab738
Nicola Ferralis <feranick@hotmail.com>
2024-03-03 17:11:52 -0500

    REfresh setup-py to conform to TF2.16.0 requirements

--- setup.py
+++ setup.py
@@ -50,12 +50,11 @@ setup(
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Topic :: Scientific/Engineering',
         'Topic :: Scientific/Engineering :: Mathematics',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
@@ -71,7 +70,7 @@ setup(
         ]
     },
     install_requires=[
-        'numpy>=1.24.0',
+        'numpy>=1.26.0',
         'Pillow>=10.0.0',
         'tflite-runtime==2.16.0-rc0',
     ],
